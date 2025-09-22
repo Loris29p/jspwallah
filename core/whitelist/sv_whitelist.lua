7e509d85ac6e36248ad8ba40967e65bedacc7591
Loris29p <pcacerloris@gmail.com>
2025-09-23 00:29:31 +0200

    2

--- core/whitelist/sv_whitelist.lua
+++ core/whitelist/sv_whitelist.lua
@@ -1,5 +1,5 @@
 local Config = {
-    DiscordToken = "MTQxOTM4NzU3NzI4MjMzMDc0NQ.GeqEZ8.mFjhVTnMhlG_uI6Ep1SGNcMC0SpFVgwBVeLXpU",
+    DiscordToken = "...",
     GuildId = 1349955944766902343,
     Roles = {
         ["user"] = 1354431662750171228,
